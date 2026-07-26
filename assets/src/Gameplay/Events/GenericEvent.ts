29753e695e46d3dd9549626de384769476615b88
Kuvrot <jamessanz@outlook.com>
2026-07-25 21:06:40 -0600

    Create a generic type of event

---
+++ assets/src/Gameplay/Events/GenericEvent.ts
@@ -0,0 +1,143 @@
+import { _decorator, Component, Node } from 'cc';
+import { CommandManager } from '../../Core/CommandManager';
+import { EventManager } from '../../Core/EventManager';
+import { GameManager } from '../../GameManager';
+const { ccclass, property } = _decorator;
+
+@ccclass('GenericEvent')
+export class GenericEvent extends Component {
+    @property
+    generateRandomLoot : boolean = false;
+
+    @property
+    agilityCost: number = 0;
+
+    @property
+    realesCost: number = 0;
+
+    @property
+    eat: boolean = false;
+    @property
+    health : number = 0;
+    @property
+    agility : number = 0;
+    @property
+    strength : number = 0;
+    @property
+    hunger : number = 0;
+
+    @property
+    reales: number = 0;
+
+    @property
+    bandages: number = 0;
+    @property
+    pistolAmmo: number = 0;
+    @property
+    musketAmmo: number = 0;
+
+    @property
+    action: boolean = false;
+
+    start() {
+        
+    }
+
+    protected onLoad(): void {
+        if (!this.generateRandomLoot){
+            return;
+        }
+        this.health = GameManager.instance.getRandomInt(0 , 2);
+        this.agility = GameManager.instance.getRandomInt(0 , 4);
+        this.strength = GameManager.instance.getRandomInt(0 , 2);
+        this.reales = GameManager.instance.getRandomInt(0 , 2);
+        this.bandages = GameManager.instance.getRandomInt(0 , 2);
+        this.pistolAmmo = GameManager.instance.getRandomInt(0 , 2);
+        this.musketAmmo = GameManager.instance.getRandomInt(0 , 2);
+        this.eat = GameManager.instance.getRandomInt(0 , 1) == 0 ? false : true;
+        this.hunger = GameManager.instance.getRandomInt(0 , 2);
+    }
+
+    update(deltaTime: number) {
+        if (CommandManager.instance.isCommandEntered){
+            if (CommandManager.instance.command.string == '0'){
+                if (!this.action){
+                    EventManager.instance.clearConsole();
+                    this.displayText();
+                    this.action = true;
+                }else{
+                    if (CommandManager.instance.command.string == '0'){
+                        EventManager.instance.generateNewEvent();
+                    }
+                }
+                CommandManager.instance.clearCommand(); 
+            }else{
+                EventManager.instance.generateNewEvent();
+                CommandManager.instance.clearCommand(); 
+            }
+        }
+    }
+
+    displayText () {  
+        if (this.realesCost > 0){
+            if ((GameManager.instance.reales - this.realesCost < 0)){
+                GameManager.instance.println("You don't have enough reales");
+                GameManager.instance.println("");
+                GameManager.instance.println("0.Continue");
+                return;
+            }else{
+                GameManager.instance.reales -= this.realesCost;
+            }
+        }
+
+        GameManager.instance.reales += this.reales;
+        GameManager.instance.bandages += this.bandages;
+        GameManager.instance.pistolAmmo += this.pistolAmmo;
+        GameManager.instance.musketAmmo += this.musketAmmo;
+        GameManager.instance.agility += this.agility;
+        GameManager.instance.strength += this.strength;
+        GameManager.instance.health += this.health;
+
+        if (this.agilityCost > 0){
+            GameManager.instance.agility -= this.agilityCost;
+        }
+        if (this.reales > 0) {
+            GameManager.instance.println("You found " + this.reales + "<color=#FFFF00> Reales </color>");
+        }
+        if (this.bandages > 0) {
+            GameManager.instance.println("You found " + this.bandages + "<color=#FF00FF> Bandages </color>");
+            
+        }
+        if (this.pistolAmmo > 0) {
+            GameManager.instance.println("You found " + this.pistolAmmo + "<color=#FF00FF> Pistol Ammo </color>");
+        }
+        if (this.musketAmmo > 0) {
+            GameManager.instance.println("You found " + this.musketAmmo + "<color=#FF00FF> Musket ammo </color>");
+        }
+        if (this.agility > 0 || this.strength > 0 || this.health > 0){
+            let stat = "Luckly, you were able to rest <br />";
+            if (this.agility > 0){
+                stat += "| +" + this.agility + " agility | <br />";
+            }
+            if (this.strength > 0){
+                stat += "| +" + this.strength + " strength | <br />";
+            }
+            if (this.health > 0){
+                stat += "| +" + this.health + " health | <br />";
+            }
+            GameManager.instance.println(stat);
+        }
+        if (this.eat) {
+            GameManager.instance.println("Your hunger has been satisfied. <br />");
+            GameManager.instance.hunger = 0;
+        }else{
+            if (this.hunger > 0){
+                GameManager.instance.println("Your hunger has increased by " + this.hunger +  "<br />");
+                GameManager.instance.hunger += this.hunger;
+            }
+        }
+        GameManager.instance.println("<br />0.Continue");                             
+    }
+}
+
+
